27cf3a1fb77b4d9d7ce97a10aefa7926117d31d6
Elliot Moffatt <40918574+ElliotMoffatt@users.noreply.github.com>
2022-01-09 17:31:49 +0000

    option to indicate orientation change on import
    added option to indicate orientation change on import

--- blender-addon-ffxiv-breast-slider.py
+++ blender-addon-ffxiv-breast-slider.py
@@ -6,12 +6,15 @@ bl_info = {
     "location": "3D View > Sidebar",
     "blender": (3, 0, 0),
     "category": "Object",
+    "support": "Community",
     "doc_url": "https://github.com/ElliotMoffatt/ffxiv-blender-breast-slider",
     "tracker_url": "https://github.com/ElliotMoffatt/ffxiv-blender-breast-slider/issues"
 }
 
 import bpy
 from mathutils import Vector
+import math
+import numpy as np
 
 c_mune_l_name = "j_mune_l"
 c_mune_r_name = "j_mune_r"
@@ -29,6 +32,9 @@ class BREASTSCALER_PT_BreastScalerPanel(bpy.types.Panel):
         layout = self.layout
         col_a = layout.column()
         col_a.prop(scene, 'selectedArmature')
+        
+        col_d = layout.column()
+        col_d.prop(scene, 'isAutomaticBoneOrientationUsed')
         
         col_b = layout.column()
         col_b.prop(scene, 'breastScalePercentage', text="Breast scale (%)", slider=True )
@@ -50,25 +56,6 @@ class BREASTSCALER_OT_BreastSizeResetOperator(bpy.types.Operator):
         context.scene.breastScalePercentage = 50
         return {'FINISHED'}
 
-
-
-def get_new_breast_width(scale):
-    return (0.16 * scale + 92) / 100
-
-def get_new_breast_depth(scale):
-    return (0.368 * scale + 81.6) / 100
-
-def get_new_breast_height(scale):
-    return (0.4 * scale + 80) / 100
-
-def get_new_breast_size_vector(scale):
-    newWidth = get_new_breast_width(scale)
-    newDepth = get_new_breast_depth(scale)
-    newHeight = get_new_breast_height(scale)
-    
-    return Vector((newHeight, newWidth, newDepth))
-
-
 def get_breast_bone_names():
     return [c_mune_l_name, c_mune_r_name]
 
@@ -79,18 +66,39 @@ def get_breast_bones(skeleton):
         bones.append(skeleton.pose.bones[name])
     return bones
 
+def get_new_breast_width(scale):
+    return (0.16 * scale + 92) / 100
+
+def get_new_breast_depth(scale):
+    return (0.368 * scale + 81.6) / 100
+
+def get_new_breast_height(scale):
+    return (0.4 * scale + 80) / 100
+
+def get_new_breast_size_vector(scale, isAutomaticBoneOrientationUsed):
+    newWidth = get_new_breast_width(scale)
+    newDepth = get_new_breast_depth(scale)
+    newHeight = get_new_breast_height(scale)
     
-def update_breast_size_on_armature(armature, breastScalePercentage):
+    if isAutomaticBoneOrientationUsed:
+        return Vector((newHeight, newWidth, newDepth))
+    else:
+        return Vector((newWidth, newDepth, newHeight))
+    
+
+
+    
+def update_breast_size_on_armature(armature, breastScalePercentage, isAutomaticBoneOrientationUsed):
     if armature:
         bones = get_breast_bones(armature)
-
+        
         for bone in bones:
-            bone.scale = get_new_breast_size_vector(breastScalePercentage)
+            bone.scale = get_new_breast_size_vector(breastScalePercentage, isAutomaticBoneOrientationUsed)
     
 
 
 def do_update_breast_size( self, context ):
-    update_breast_size_on_armature(context.scene.selectedArmature, self.breastScalePercentage)
+    update_breast_size_on_armature(context.scene.selectedArmature, self.breastScalePercentage, self.isAutomaticBoneOrientationUsed)
     
 
 def register():
@@ -98,6 +106,13 @@ def register():
     bpy.types.Scene.selectedArmature = bpy.props.PointerProperty(
         type = bpy.types.Object,
         name = "Armature to scale Breasts",
+        update = do_update_breast_size
+    )
+    
+    bpy.types.Scene.isAutomaticBoneOrientationUsed = bpy.props.BoolProperty(
+        default = False,
+        name = "'Automatic bone orientation' used",
+        description = "Select this if you selected the 'Armature > Automatic Bone Orientation' option when importing",
         update = do_update_breast_size
     )
     
@@ -120,3 +135,4 @@ def unregister():
     bpy.utils.unregister_class(BREASTSCALER_OT_BreastSizeResetOperator)
     del bpy.types.Scene.breastScalePercentage
     del bpy.types.Scene.selectedArmature
+    del bpy.types.Scene.isAutomaticBoneOrientationUsed
